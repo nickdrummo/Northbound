d30149c3e10c2362784f3fa388ce1457394be46f
Eric Zhou <ezho187@gmail.com>
2026-03-13 19:21:50 +1100

    feat(NORTH-49): update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import healthRouter from './health/health.router';
+import ordersRouter from './orders/orders.router';
 
 const app = express();
 
 app.use(express.json());
 app.use('/health', healthRouter);
+app.use('/v1/orders', ordersRouter);
 
 export default app;
