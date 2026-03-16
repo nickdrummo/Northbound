0fc5aa5c53f2033a810903e5367f482a1ff81545
Nick <nick.drummond@icloud.com>
2026-03-16 12:24:33 +1100

    Added package to help fix pathway issues

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 import healthRouter from './health/health.router';
 import authRouter from './auth/routes';
 import ordersRouter from './orders/orders.router';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/health', healthRouter);
 app.use(authRouter);
